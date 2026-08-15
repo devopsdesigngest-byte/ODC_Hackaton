028aad8f3de54939a68b2ef9181f1fd699fd5f93
Ndiasse Mbow <NdiasseMbow@gmail.com>
2026-08-15 03:13:43 +0000

    feat(core): implementation de Database Singleton avec fallback automatique PostgreSQL vers SQLite

--- DEVLOG.md
+++ DEVLOG.md
@@ -37,3 +37,14 @@ je me perdais aussi au milieu de beaucoup de classes
 J'ai remarqué que PostgreSQL et SQLite sont assez similaires dans la manière de créer les tables et les relations. La principale différence que j'ai remarquée concerne la gestion des identifiants auto-incrémentés.
 Au début, je n'avais pas compris qu'il fallait également établir une connexion avec SQLite comme on le fait avec PostgreSQL, puisque ce sont toutes les deux des bases de données.
 Cette partie m'a permis de mieux comprendre le principe du fallback entre PostgreSQL et SQLite et pourquoi l'application doit pouvoir utiliser l'une ou l'autre.
+
+
+
+
+
+### 🌃 [Vendredi - Phase 1] : Conception & BDD Fallback
+- **Heure de réalisation** : 03 h 00
+- **Ce qui a été fait** : Mise en place du Singleton Database avec connexion PostgreSQL et fallback automatique vers SQLite
+- **Difficultés / Obstacles** : 
+J’ai eu du mal à comprendre le fonctionnement du Singleton au début.
+J’ai également dû comprendre comment utiliser PDO pour se connecter à PostgreSQL(deja connu) et SQLite.
---
+++ docs/BD/src/Core/Database.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Core;
+
+use PDO;
+use PDOException;
+
+class Database {
+    private static ?Database $instance = null;
+
+    private PDO $connection;
+
+    private function __construct()     {
+        try {
+            $this->connection = new PDO(
+                'pgsql:host=localhost;port=5432;dbname=erp', 'postgres', 'postgres');
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            $this->connection = new PDO(
+                'sqlite:' . __DIR__ . '/../../erp.db'
+            );
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connection->exec(
+                'PRAGMA foreign_keys = ON'
+            );
+        }
+    }
+    public static function getInstance(): Database {
+        if (self::$instance === null) {
+            self::$instance = new Database();
+        }
+        return self::$instance;
+    }
+
+    public function getConnection(): PDO {
+        return $this->connection;
+    }
+}
