d0b2ed37291b533a837e2f0a83196a7e4246918e
Ndiasse Mbow <NdiasseMbow@gmail.com>
2026-08-18 06:02:45 +0000

    feat(pos): finalisation du controleur et de l'interface de caisse tactile

--- src/Controller/POSController.php
+++ src/Controller/POSController.php
@@ -7,37 +7,93 @@ require_once dirname(__DIR__) . '/Service/VenteService.php';
 
 class POSController {
 
-    public function showVue(): void {
+    public static function showVue(): void {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             if ($_POST['btn'] === 'panier') 
-                $this->ajouterDansPanier();      
+                self::ajouterDansPanier();      
             if ($_POST['btn'] === 'valider') 
-                $this->enregistrerVente();
+                self::enregistrerVente();
         }
-        $insClient = new ClientRepository();
-        $clients = $insClient->getAllClient();
-        $insProduit = new ProduitRepository();
-        $produits = $insProduit->getAllProduit();
-        $insMode = new ModePaiementRepository();
-        $modes = $insMode->getAllModePaiement();
+
+        $clients = ClientRepository::getAllClient();
+        $produits = ProduitRepository::getAllProduit();
+        $modes = ModePaiementRepository::getAllModePaiement();
         $panier = $_SESSION['panier'] ?? [];        
+
         require_once dirname(__DIR__) . '/views/pos/index.php';
     }
 
 
-    public function ajouterDansPanier() : void {
+    public static function ajouterDansPanier(): void {
         $produit = explode('|', $_POST['produit_id']);
         $quantite = (int) $_POST['quantite'];
+
         if (!isset($_SESSION['panier'])) {
             $_SESSION['panier'] = [];
         }
-        $_SESSION['panier'][] = ['produitId' => (int) $produit[0], 'prixUnitaire' => (float) $produit[1], 'stock' => (int) $produit[2], 'libelle' => $produit[3], 'quantite' => $quantite, 'sousTotal' => (float) $produit[1] * $quantite];
+
+        $_SESSION['panier'][] = [
+            'produitId' => (int) $produit[0],
+            'prixUnitaire' => (float) $produit[1],
+            'stock' => (int) $produit[2],
+            'libelle' => $produit[3],
+            'quantite' => $quantite,
+            'sousTotal' => (float) $produit[1] * $quantite
+        ];
     }
 
 
-    public function enregistrerVente() : void {
-        $venteService = new VenteService();
-        $venteService->enregistrerVente((int) $_POST['client_id'], (int) $_SESSION['utilisateur_id'], (int) $_POST['mode_reglement'], (float) $_POST['montant_verse'], $_SESSION['panier']);
+    public static function enregistrerVente(): void {
+        VenteService::enregistrerVente(
+            (int) $_POST['client_id'],
+            (int) $_SESSION['utilisateur_id'],
+            (int) $_POST['mode_reglement'],
+            (float) $_POST['montant_verse'],
+            $_SESSION['panier']
+        );
+
         unset($_SESSION['panier']);
     }
 }
+
+// require_once dirname(__DIR__) . '/Model/Repository/ClientRepository.php';
+// require_once dirname(__DIR__) . '/Model/Repository/ProduitRepository.php';
+// require_once dirname(__DIR__) . '/Model/Repository/ModePaiementRepository.php';
+// require_once dirname(__DIR__) . '/Service/VenteService.php';
+
+// class POSController {
+
+//     public function showVue(): void {
+//         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+//             if ($_POST['btn'] === 'panier') 
+//                 $this->ajouterDansPanier();      
+//             if ($_POST['btn'] === 'valider') 
+//                 $this->enregistrerVente();
+//         }
+//         $insClient = new ClientRepository();
+//         $clients = $insClient->getAllClient();
+//         $insProduit = new ProduitRepository();
+//         $produits = $insProduit->getAllProduit();
+//         $insMode = new ModePaiementRepository();
+//         $modes = $insMode->getAllModePaiement();
+//         $panier = $_SESSION['panier'] ?? [];        
+//         require_once dirname(__DIR__) . '/views/pos/index.php';
+//     }
+
+
+//     public function ajouterDansPanier() : void {
+//         $produit = explode('|', $_POST['produit_id']);
+//         $quantite = (int) $_POST['quantite'];
+//         if (!isset($_SESSION['panier'])) {
+//             $_SESSION['panier'] = [];
+//         }
+//         $_SESSION['panier'][] = ['produitId' => (int) $produit[0], 'prixUnitaire' => (float) $produit[1], 'stock' => (int) $produit[2], 'libelle' => $produit[3], 'quantite' => $quantite, 'sousTotal' => (float) $produit[1] * $quantite];
+//     }
+
+
+//     public function enregistrerVente() : void {
+//         $venteService = new VenteService();
+//         $venteService->enregistrerVente((int) $_POST['client_id'], (int) $_SESSION['utilisateur_id'], (int) $_POST['mode_reglement'], (float) $_POST['montant_verse'], $_SESSION['panier']);
+//         unset($_SESSION['panier']);
+//     }
+// }
