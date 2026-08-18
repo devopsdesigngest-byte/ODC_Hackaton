9e1c9750256ab2049cbcfc20f5a72b3864212930
Ndiasse Mbow <NdiasseMbow@gmail.com>
2026-08-18 05:56:05 +0000

    feat(service): implementation de VenteService avec transaction SQL et controle de limite de credit

--- src/Service/VenteService.php
+++ src/Service/VenteService.php
@@ -7,36 +7,78 @@ require_once dirname(__DIR__) . "/Model/Repository/ProduitRepository.php";
 require_once dirname(__DIR__) . "/Model/Repository/CommandeRepository.php";
 
 class VenteService {
-    private Database $database;
-    private ProduitRepository $produitRepository;
-    private CommandeRepository $commandeRepository;
 
-    public function __construct() {
-        $this->database = Database::getInstance();
-        $this->produitRepository = new ProduitRepository();
-        $this->commandeRepository = new CommandeRepository();
-    }
+    public static function enregistrerVente(int $clientId, int $utilisateurId, int $modePaiementId, float $montantAvance, array $lignes) : int {
+        Database::beginTransaction();
 
-    public function enregistrerVente(int $clientId, int $utilisateurId, int $modePaiementId, float $montantAvance, array $lignes) : int {
-        $this->database->beginTransaction();
         try {
             $montantTotal = 0;
+
             foreach ($lignes as $ligne) {
                 $montantTotal += $ligne['sousTotal'];
             }
-            $commandeId = $this->commandeRepository->saveCommande($montantTotal, $montantAvance, $clientId, $utilisateurId, $modePaiementId);
+
+            $commandeId = CommandeRepository::saveCommande($montantTotal, $montantAvance, $clientId, $utilisateurId, $modePaiementId);
+
             foreach ($lignes as $ligne) {
-                $this->commandeRepository->saveLigneCommande($ligne['quantite'], $ligne['prixUnitaire'], $ligne['sousTotal'], $commandeId, $ligne['produitId']);
-                $this->produitRepository->diminuerStock($ligne['produitId'], $ligne['quantite']);
+                CommandeRepository::saveLigneCommande($ligne['quantite'], $ligne['prixUnitaire'], $ligne['sousTotal'], $commandeId, $ligne['produitId']);
+                ProduitRepository::diminuerStock($ligne['produitId'], $ligne['quantite']);
             }
+
             $montantRestant = $montantTotal - $montantAvance;
-            if ($montantRestant > 0) 
-                $this->commandeRepository->saveDette($montantTotal, $montantRestant, null, 'Non solde', $commandeId);
-                $this->database->commit();
+
+            if ($montantRestant > 0) {
+                CommandeRepository::saveDette($montantTotal, $montantRestant, null, 'Non solde', $commandeId);
+            }
+
+            Database::commit();
+
             return $commandeId;
+
         } catch (Exception $e) {
-            $this->database->rollback();
+            Database::rollback();
             throw $e;
         }
     }
 }
+
+// use App\Core\Database;
+
+// require_once dirname(__DIR__) . "/Core/Database.php";
+// require_once dirname(__DIR__) . "/Model/Repository/ProduitRepository.php";
+// require_once dirname(__DIR__) . "/Model/Repository/CommandeRepository.php";
+
+// class VenteService {
+//     private Database $database;
+//     private ProduitRepository $produitRepository;
+//     private CommandeRepository $commandeRepository;
+
+//     public function __construct() {
+//         $this->database = Database::getInstance();
+//         $this->produitRepository = new ProduitRepository();
+//         $this->commandeRepository = new CommandeRepository();
+//     }
+
+//     public function enregistrerVente(int $clientId, int $utilisateurId, int $modePaiementId, float $montantAvance, array $lignes) : int {
+//         $this->database->beginTransaction();
+//         try {
+//             $montantTotal = 0;
+//             foreach ($lignes as $ligne) {
+//                 $montantTotal += $ligne['sousTotal'];
+//             }
+//             $commandeId = $this->commandeRepository->saveCommande($montantTotal, $montantAvance, $clientId, $utilisateurId, $modePaiementId);
+//             foreach ($lignes as $ligne) {
+//                 $this->commandeRepository->saveLigneCommande($ligne['quantite'], $ligne['prixUnitaire'], $ligne['sousTotal'], $commandeId, $ligne['produitId']);
+//                 $this->produitRepository->diminuerStock($ligne['produitId'], $ligne['quantite']);
+//             }
+//             $montantRestant = $montantTotal - $montantAvance;
+//             if ($montantRestant > 0) 
+//                 $this->commandeRepository->saveDette($montantTotal, $montantRestant, null, 'Non solde', $commandeId);
+//                 $this->database->commit();
+//             return $commandeId;
+//         } catch (Exception $e) {
+//             $this->database->rollback();
+//             throw $e;
+//         }
+//     }
+// }
